9f190918aca32abba0b7fd0bf3a0f760ef901c75
Hafsa7676 <mahmoodhafsa76@gmail.com>
2026-04-10 14:06:53 +0500

    Time: 0 ms (100.00%) | Memory: 19.2 MB (91.99%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,14 +1,8 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        count = Counter(nums)
+        count = defaultdict(int)
         res = 0
-        for num, c in count.items():
-
-            # // is used to keep everything as int.
-            #  In c * (c-1), one of c or c-1 is always even (they are consecutive integers, so onemust        #    be even). That means the product is always divisible by 2 with no remainder.
-            res += c * (c - 1) // 2
-
-            # the above formula is the simplified version of nCr 
-
-
+        for num in nums:
+            res += count[num]
+            count[num] += 1
         return res
