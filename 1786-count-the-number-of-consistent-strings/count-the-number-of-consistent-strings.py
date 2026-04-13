41d49fa88fba4b1166d6bcf86f23152265e1c60c
Hafsa7676 <mahmoodhafsa76@gmail.com>
2026-04-13 14:09:59 +0500

    Time: 236 ms (18.10%) | Memory: 20.9 MB (56.53%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -1,13 +1,14 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        allowed_array = [False] * 26
-        for c in allowed:
-            allowed_array[ord(c) - ord('a')] = True                        
+        bitmask = 0
+        for char in allowed:
+            bitmask = bitmask | (1 << (ord(char) - ord('a')))
 
         res = len(words)
-        for w in words:
-            for c in w:
-                if not allowed_array[ord(c) - ord('a')]:
+        for word in words:
+            for w in word:
+                bit_pos = 1 <<  (ord(w) - ord('a'))
+                if bit_pos & bitmask == 0:
                     res -= 1
                     break
 
