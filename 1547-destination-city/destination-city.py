c7de093aa0ec74f3a139b9289b1576d929389b6e
Hafsa7676 <mahmoodhafsa76@gmail.com>
2026-04-14 16:43:19 +0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.21%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,15 +1,13 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
         s = set()
-        for i in range(len(paths)):
-            for j in range(2):
-                s.add(paths[i][0])
+        for p in paths:
+            s.add(p[0])
 
 
-        for i in range(len(paths)):
-            for j in range((2)):
-                if paths[i][1] not in s:
-                    return paths[i][1]
+        for p in paths:
+            if p[1] not in s:
+                return p[1]
 
 
 
