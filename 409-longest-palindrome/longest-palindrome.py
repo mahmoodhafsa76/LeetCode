718a3eb9754c480eceacd6fe4045e8193949e4c5
Hafsa7676 <mahmoodhafsa76@gmail.com>
2026-04-24 05:10:39 +0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (93.42%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,17 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        count = defaultdict(int)
-        res = 0
+        count = set()
+        result = 0
 
-        for c in s:
-            count[c] += 1
-            if count[c] % 2 == 0:
-                res += 2
+        for char in s:
+            if char in count:
+                count -= {char}
+                result += 2
 
-        if res < len(s):
-            res += 1
-            
-        return res
+            else:
+                count.add(char)
+        
+        if count:
+            result += 1
 
+        return result
 
         
