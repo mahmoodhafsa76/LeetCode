43e410d2c0260d4475430b2e12894086e4beb255
Hafsa7676 <mahmoodhafsa76@gmail.com>
2026-04-26 12:34:47 +0500

    Time: 3 ms (31.47%) | Memory: 19.1 MB (89.44%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
@@ -4,7 +4,7 @@ class Solution:
         index = [-1] * 26
        
         for i in range(len(s)):
-            j = ord(s[i]) - ord('a')
+            j = ord(s[i]) - ord('a')  # j is the represents indices of index array
             if index[j] != -1:
                 res = max(res, i - index[j] - 1)
             else:
