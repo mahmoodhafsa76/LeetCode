8b078db05aa25e24eeda052410a9e30ba010b906
Hafsa7676 <mahmoodhafsa76@gmail.com>
2026-05-21 11:42:45 +0500

    Time: 57 ms (71.43%) | Memory: 32.4 MB (95.71%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -1,16 +1,26 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
-        left = defaultdict(int)
-        right = Counter(nums)
+        majority = 0 
+        count = 0 
+
+        for n in nums:
+            if count == 0:
+                majority = n
+            if n == majority:
+                count += 1
+            else:
+                count -= 1
+
+        left_count = 0
+        right_count = nums.count(majority)
 
         for i in range(len(nums)):
-            left[nums[i]] += 1
-            right[nums[i]] -= 1
-
-            left_len = i + 1
+            if nums[i] == majority:
+                left_count += 1
+                right_count -= 1
+            left_len = i+1
             right_len = len(nums) - i - 1
-
-            if 2 * left[nums[i]] > left_len and 2 * right[nums[i]] > right_len:
-                return i 
+            if 2 * left_count > left_len and 2 * right_count > right_len:
+                return i
 
         return -1
